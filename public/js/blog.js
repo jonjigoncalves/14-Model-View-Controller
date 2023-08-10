1201020ac2032471084c3d657c651e94f26d90b8
Jonathan Goncalves <jonjigoncalves@gmail.com>
2023-08-10 10:07:18 -0400

    fixed isues

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,27 +1,27 @@
-const newCommentSave = async (event) => {
-    event.preventDefault();
+// const newCommentSave = async (event) => {
+//     event.preventDefault();
 
-    const blog_id = event.target.getAttribute('data-id');
+//     const blog_id = event.target.getAttribute('data-id');
 
-    if(event.target.hasAttribute('data-id')){
+//     if(event.target.hasAttribute('data-id')){
 
-        const comment = document.querySelector(`#blog-comment${blog_id}`).value.trim();
+//         const comment = document.querySelector(`#blog-comment${blog_id}`).value.trim();
 
-        console.log(comment)
+//         console.log(comment)
 
-        if(comment){
-            const response = await fetch('/api/comment', {
-                method: 'POST',
-                body: JSON.stringify({ blog_id, comment }),
-                headers: { 'Content-Type': 'application/json' },
-            });
+//         if(comment){
+//             const response = await fetch('/api/comment', {
+//                 method: 'POST',
+//                 body: JSON.stringify({ blog_id, comment }),
+//                 headers: { 'Content-Type': 'application/json' },
+//             });
 
-            if(response.ok){
-                document.location.replace('/dashboard')
-            } else {
-                alert('something is wrong')
-            }
-        }
-    }
-};
-document.querySelector('#commentBtn').addEventListener('click', newCommentSave);
+//             if(response.ok){
+//                 document.location.replace('/dashboard')
+//             } else {
+//                 alert('something is wrong')
+//             }
+//         }
+//     }
+// };
+// document.querySelector('#commentBtn').addEventListener('click', newCommentSave);
